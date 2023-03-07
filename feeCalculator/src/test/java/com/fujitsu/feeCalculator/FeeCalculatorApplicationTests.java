ca428e05fc49f5d03b64af9ca803e5b8a260cfc6
edvildt <edgar.vildt@gmail.com>
2023-03-07 11:40:27 +0200

    fee calculator initial commit. Project generated

---
+++ feeCalculator/src/test/java/com/fujitsu/feeCalculator/FeeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fujitsu.feeCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
